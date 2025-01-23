73dbc77d5be44873ef532c9dcab264e927d26356
Developer-dreamer <podolancukserafim@gmail.com>
2025-01-23 15:56:51 +0200

    a little refactoring

--- CalculatorTests/BaseCases.cs
+++ CalculatorTests/BaseCases.cs
@@ -1,6 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Globalization;
-using NUnit.Framework;
 
 namespace CalculatorTests;
 
@@ -12,52 +10,52 @@ public class Tests
         "Calculator/bin/Debug/net9.0/Calculator"
     );
 
-    private static IEnumerable<object[]> _tokenizationTestCases = new List<object[]>
+    private static IEnumerable<string[]> _tokenizationTestCases = new List<string[]>
     {
-        new object[] { "3", "3" },
-        new object[] { "3+5", "3, +, 5" },
-        new object[] { "3 +    5 ", "3, +, 5" },
-        new object[] { "3 + 5 + 9", "3, +, 5, +, 9" },
-        new object[] { "7 * 8 + 3 * 6", "7, *, 8, +, 3, *, 6" },
-        new object[] { "7*8+3*6", "7, *, 8, +, 3, *, 6" },
-        new object[] { "3+8/2-9*3*3*3", "3, +, 8, /, 2, -, 9, *, 3, *, 3, *, 3" },
-        new object[] { "7*(8+3)*6", "7, *, (, 8, +, 3, ), *, 6" },
-        new object[] { " 7 *(8+ 3)  *6", "7, *, (, 8, +, 3, ), *, 6" },
-        new object[] { " sin(7)", "sin, (, 7, )" },
-        new object[] { "4 + sin(7)^9", "4, +, sin, (, 7, ), ^, 9" },
-        new object[] { "pow(4; 3)", "pow, (, 4, ;, 3, )" }
+        new [] { "3", "3" },
+        new [] { "3+5", "3, +, 5" },
+        new [] { "3 +    5 ", "3, +, 5" },
+        new [] { "3 + 5 + 9", "3, +, 5, +, 9" },
+        new [] { "7 * 8 + 3 * 6", "7, *, 8, +, 3, *, 6" },
+        new [] { "7*8+3*6", "7, *, 8, +, 3, *, 6" },
+        new [] { "3+8/2-9*3*3*3", "3, +, 8, /, 2, -, 9, *, 3, *, 3, *, 3" },
+        new [] { "7*(8+3)*6", "7, *, (, 8, +, 3, ), *, 6" },
+        new [] { " 7 *(8+ 3)  *6", "7, *, (, 8, +, 3, ), *, 6" },
+        new [] { " sin(7)", "sin, (, 7, )" },
+        new [] { "4 + sin(7)^9", "4, +, sin, (, 7, ), ^, 9" },
+        new [] { "pow(4; 3)", "pow, (, 4, ;, 3, )" }
     };
     
-    private static IEnumerable<object[]> _rpnTestCases = new List<object[]>
+    private static IEnumerable<string[]> _rpnTestCases = new List<string[]>
     {
-        new object[] { "3", "3" },
-        new object[] { "3+5", "3, 5, +" },
-        new object[] { "3 +    5 ", "3, 5, +" },
-        new object[] { "3 + 5 + 9", "3, 5, +, 9, +" },
-        new object[] { "7 * 8 + 3 * 6", "7, 8, *, 3, 6, *, +" },
-        new object[] { "7*8+3*6", "7, 8, *, 3, 6, *, +" },
-        new object[] { "3+8/2-9*3*3*3", "3, 8, 2, /, +, 9, 3, *, 3, *, 3, *, -" },
-        new object[] { "7*(8+3)*6", "7, 8, 3, +, *, 6, *" },
-        new object[] { " 7 *(8+ 3)  *6", "7, 8, 3, +, *, 6, *" },
-        new object[] { " sin(7)", "7, sin" },
-        new object[] { "4 + sin(7)^9", "4, 7, sin, 9, ^, +" },
-        new object[] { "pow(4; 3)", "4, 3, pow" }
+        new [] { "3", "3" },
+        new [] { "3+5", "3, 5, +" },
+        new [] { "3 +    5 ", "3, 5, +" },
+        new [] { "3 + 5 + 9", "3, 5, +, 9, +" },
+        new [] { "7 * 8 + 3 * 6", "7, 8, *, 3, 6, *, +" },
+        new [] { "7*8+3*6", "7, 8, *, 3, 6, *, +" },
+        new [] { "3+8/2-9*3*3*3", "3, 8, 2, /, +, 9, 3, *, 3, *, 3, *, -" },
+        new [] { "7*(8+3)*6", "7, 8, 3, +, *, 6, *" },
+        new [] { " 7 *(8+ 3)  *6", "7, 8, 3, +, *, 6, *" },
+        new [] { " sin(7)", "7, sin" },
+        new [] { "4 + sin(7)^9", "4, 7, sin, 9, ^, +" },
+        new [] { "pow(4; 3)", "4, 3, pow" }
     };
     
-    private static IEnumerable<object[]> _evaluationTestCases = new List<object[]>
+    private static IEnumerable<string[]> _evaluationTestCases = new List<string[]>
     {
-        new object[] { "3", "3" },
-        new object[] { "3+5", "8" },
-        new object[] { "3 +    5 ", "8" },
-        new object[] { "3 + 5 + 9", "17" },
-        new object[] { "7 * 8 + 3 * 6", "74" },
-        new object[] { "7*8+3*6", "74" },
-        new object[] { "3+8/2-9*3*3*3", "-236" },
-        new object[] { "7*(8+3)*6", "462" },
-        new object[] { " 7 *(8+ 3)  *6", "462" },
-        new object[] { " sin(7)", "0.656986598718789" },
-        new object[] { "4 + sin(7)^9", "4.0228038721672474" },
-        new object[] { "pow(4; 3)", "64" }
+        new [] { "3", "3" },
+        new [] { "3+5", "8" },
+        new [] { "3 +    5 ", "8" },
+        new [] { "3 + 5 + 9", "17" },
+        new [] { "7 * 8 + 3 * 6", "74" },
+        new [] { "7*8+3*6", "74" },
+        new [] { "3+8/2-9*3*3*3", "-236" },
+        new [] { "7*(8+3)*6", "462" },
+        new [] { " 7 *(8+ 3)  *6", "462" },
+        new [] { " sin(7)", "0.656986598718789" },
+        new [] { "4 + sin(7)^9", "4.0228038721672474" },
+        new [] { "pow(4; 3)", "64" }
     };
     
     private async Task<string> RunCalculatorAsync(string input)
@@ -69,7 +67,7 @@ public class Tests
             RedirectStandardOutput = true,
             RedirectStandardError = true,
             UseShellExecute = false,
-            CreateNoWindow = true
+            CreateNoWindow = false
         };
 
         using (var process = new Process { StartInfo = processStartInfo })
